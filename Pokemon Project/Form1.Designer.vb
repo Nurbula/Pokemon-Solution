116e0bde085d471d57fb4c4dfde81f2aa1e01b66
Griffin Dyer <GDyer8913@slps.org>
2023-04-10 19:15:30 -0500

    Add project files.

---
+++ Pokemon Project/Form1.Designer.vb
@@ -0,0 +1,250 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.chkGen1 = New System.Windows.Forms.CheckBox()
+        Me.chkGen2 = New System.Windows.Forms.CheckBox()
+        Me.chkGen3 = New System.Windows.Forms.CheckBox()
+        Me.chkGen4 = New System.Windows.Forms.CheckBox()
+        Me.chkGen5 = New System.Windows.Forms.CheckBox()
+        Me.chkGen6 = New System.Windows.Forms.CheckBox()
+        Me.chkGen7 = New System.Windows.Forms.CheckBox()
+        Me.chkGen8 = New System.Windows.Forms.CheckBox()
+        Me.chkGen9 = New System.Windows.Forms.CheckBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(726, 70)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(118, 145)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Confirm"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(726, 270)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(118, 145)
+        Me.btnClose.TabIndex = 3
+        Me.btnClose.Text = "Exit"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(12, 24)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(270, 404)
+        Me.ListBox1.TabIndex = 4
+        Me.ListBox1.Visible = False
+        '
+        'chkGen1
+        '
+        Me.chkGen1.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.chkGen1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.chkGen1.Location = New System.Drawing.Point(63, 83)
+        Me.chkGen1.Name = "chkGen1"
+        Me.chkGen1.Size = New System.Drawing.Size(112, 111)
+        Me.chkGen1.TabIndex = 0
+        Me.chkGen1.Text = "Generation 1"
+        Me.chkGen1.UseVisualStyleBackColor = False
+        '
+        'chkGen2
+        '
+        Me.chkGen2.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.chkGen2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.chkGen2.Location = New System.Drawing.Point(181, 83)
+        Me.chkGen2.Name = "chkGen2"
+        Me.chkGen2.Size = New System.Drawing.Size(112, 111)
+        Me.chkGen2.TabIndex = 1
+        Me.chkGen2.Text = "Generation 2"
+        Me.chkGen2.UseVisualStyleBackColor = False
+        '
+        'chkGen3
+        '
+        Me.chkGen3.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.chkGen3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.chkGen3.Location = New System.Drawing.Point(299, 83)
+        Me.chkGen3.Name = "chkGen3"
+        Me.chkGen3.Size = New System.Drawing.Size(112, 111)
+        Me.chkGen3.TabIndex = 2
+        Me.chkGen3.Text = "Generation 3"
+        Me.chkGen3.UseVisualStyleBackColor = False
+        '
+        'chkGen4
+        '
+        Me.chkGen4.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.chkGen4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.chkGen4.Location = New System.Drawing.Point(63, 200)
+        Me.chkGen4.Name = "chkGen4"
+        Me.chkGen4.Size = New System.Drawing.Size(112, 111)
+        Me.chkGen4.TabIndex = 3
+        Me.chkGen4.Text = "Generation 4"
+        Me.chkGen4.UseVisualStyleBackColor = False
+        '
+        'chkGen5
+        '
+        Me.chkGen5.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.chkGen5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.chkGen5.Location = New System.Drawing.Point(181, 200)
+        Me.chkGen5.Name = "chkGen5"
+        Me.chkGen5.Size = New System.Drawing.Size(112, 111)
+        Me.chkGen5.TabIndex = 4
+        Me.chkGen5.Text = "Generation 5"
+        Me.chkGen5.UseVisualStyleBackColor = False
+        '
+        'chkGen6
+        '
+        Me.chkGen6.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.chkGen6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.chkGen6.Location = New System.Drawing.Point(299, 200)
+        Me.chkGen6.Name = "chkGen6"
+        Me.chkGen6.Size = New System.Drawing.Size(112, 111)
+        Me.chkGen6.TabIndex = 5
+        Me.chkGen6.Text = "Generation 6"
+        Me.chkGen6.UseVisualStyleBackColor = False
+        '
+        'chkGen7
+        '
+        Me.chkGen7.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.chkGen7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.chkGen7.Location = New System.Drawing.Point(63, 317)
+        Me.chkGen7.Name = "chkGen7"
+        Me.chkGen7.Size = New System.Drawing.Size(112, 111)
+        Me.chkGen7.TabIndex = 6
+        Me.chkGen7.Text = "Generation 7"
+        Me.chkGen7.UseVisualStyleBackColor = False
+        '
+        'chkGen8
+        '
+        Me.chkGen8.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.chkGen8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.chkGen8.Location = New System.Drawing.Point(181, 317)
+        Me.chkGen8.Name = "chkGen8"
+        Me.chkGen8.Size = New System.Drawing.Size(112, 111)
+        Me.chkGen8.TabIndex = 7
+        Me.chkGen8.Text = "Generation 8"
+        Me.chkGen8.UseVisualStyleBackColor = False
+        '
+        'chkGen9
+        '
+        Me.chkGen9.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.chkGen9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.chkGen9.Location = New System.Drawing.Point(299, 317)
+        Me.chkGen9.Name = "chkGen9"
+        Me.chkGen9.Size = New System.Drawing.Size(112, 111)
+        Me.chkGen9.TabIndex = 8
+        Me.chkGen9.Text = "Generation 9"
+        Me.chkGen9.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(299, 27)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 9
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(726, 99)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(118, 145)
+        Me.btnNext.TabIndex = 10
+        Me.btnNext.Text = "Next"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(726, 240)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(118, 145)
+        Me.btnReset.TabIndex = 11
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(726, 382)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(118, 33)
+        Me.btnExit.TabIndex = 12
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(886, 467)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.chkGen9)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.chkGen8)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.chkGen7)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.chkGen6)
+        Me.Controls.Add(Me.chkGen5)
+        Me.Controls.Add(Me.chkGen3)
+        Me.Controls.Add(Me.chkGen4)
+        Me.Controls.Add(Me.chkGen1)
+        Me.Controls.Add(Me.chkGen2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents chkGen1 As CheckBox
+    Friend WithEvents chkGen2 As CheckBox
+    Friend WithEvents chkGen3 As CheckBox
+    Friend WithEvents chkGen4 As CheckBox
+    Friend WithEvents chkGen5 As CheckBox
+    Friend WithEvents chkGen6 As CheckBox
+    Friend WithEvents chkGen7 As CheckBox
+    Friend WithEvents chkGen8 As CheckBox
+    Friend WithEvents chkGen9 As CheckBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnNext As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Pokemon Project/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles chkGen1.CheckedChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        chkGen1.Visible = False
+        chkGen2.Visible = False
+        chkGen3.Visible = False
+        chkGen4.Visible = False
+        chkGen5.Visible = False
+        chkGen6.Visible = False
+        chkGen7.Visible = False
+        chkGen8.Visible = False
+        chkGen9.Visible = False
+        ListBox1.Visible = True
+        Button1.Visible = False
+        btnClose.Visible = False
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+
+    End Sub
+End Class
